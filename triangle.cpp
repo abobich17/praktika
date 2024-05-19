60d9350a6743f62a5657a1ce593b07e6d58074ee
abobich17 <aleksejlegostaev6@gmail.com>
2024-05-19 23:56:45 +0700

    da

---
+++ triangle.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	int temp;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		temp = i;
+		while (temp != 0) {
+			cout << "*";
+			temp--;
+		}
+		cout << "*" << endl;
+	}
+}
+
